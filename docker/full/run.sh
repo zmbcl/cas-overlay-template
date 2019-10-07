8f8e0f0e67cc81dedd5b1aa2b60b188276aeb819
hdeadman <hal.deadman@gmail.com>
2019-10-06 23:33:28 -0400

    comment out some dependencies
    trying to figure out why get no response when browsing to app

--- docker/full/run.sh
+++ docker/full/run.sh
@@ -3,4 +3,5 @@ CONTAINER_NAME=${1:-cas}
 image_tag=(`cat ../../gradle.properties | grep "cas.version" | cut -d= -f2`)
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
-docker run --rm -p 8443:8443 -p 8080:8080 --name $CONTAINER_NAME org.apereo.cas/cas:$image_tag
+docker run --rm -d -p 9010:9010 -p 8443:8443 -p 8080:8080 --name $CONTAINER_NAME org.apereo.cas/cas:$image_tag
+docker logs cas -f
--- docker/full/start_cas.sh
+++ docker/full/start_cas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DEFAULT_JVM_ARGS=-Xmx2048M
-JVM_ARGS=${JVM_ARGS:-$DEFAULT_JVM_ARGS}
+DEFAULT_JVM_ARGS="-Xms1500M -Xmx3000M"
+RESULT_JVM_ARGS=${JVM_ARGS:-$DEFAULT_JVM_ARGS}
 
 CLOUD_VAULT_CONFIG=${CLOUD_VAULT_CONFIG:-false}
 CLOUD_CONSUL_CONFIG=${CLOUD_CONSUL_CONFIG:-false}
@@ -26,4 +26,10 @@ fi
 
 # turn off some things, mostly cloud config options, make configurable later
 CAS_ARGS="--spring.cloud.zookeeper.enabled=${CLOUD_ZOOKEEPER_CONFIG} --spring.cloud.amqp.enabled=false --spring.cloud.vault.enabled=${CLOUD_VAULT_CONFIG} --spring.cloud.consul.enabled=${CLOUD_CONSUL_CONFIG} --eureka.client.enabled=false --spring.zipkin.enabled=false --spring.sleuth.enabled=false --ribbon.eureka.enabled=false --feign.hystrix.enabled=false"
-exec /opt/java/openjdk/bin/java -server -noverify -Dloader.path=WEB-INF/classes,WEB-INF/lib,/cas-overlay/libs $JVM_ARGS -jar cas.war  $CAS_ARGS
+exec /opt/java/openjdk/bin/java -Dcom.sun.management.jmxremote \
+  -Dcom.sun.management.jmxremote.port=9010 \
+  -Dcom.sun.management.jmxremote.local.only=false \
+  -Dcom.sun.management.jmxremote.authenticate=false \
+  -Dcom.sun.management.jmxremote.ssl=false \
+  -server -noverify \
+  -Dloader.path=WEB-INF/classes,WEB-INF/lib,/cas-overlay/libs $RESULT_JVM_ARGS -jar cas.war $CAS_ARGS
